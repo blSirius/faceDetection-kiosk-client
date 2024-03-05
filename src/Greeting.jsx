12ac14a1d7c364325c476d563c880f9b31440f2d
blSirius <siriusblackazka@gmail.com>
2024-03-05 16:34:36 +0700

    5/3/2024

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export function Greeting({ getFaceDataSignal }) {
+function Greeting({ getFaceDataSignal }) {
     const [faceData, setFaceData] = useState([]);
 
     const fetchFaceData = async () => {
